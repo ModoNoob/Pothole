77f2ecd1f472aa91a7c44e51e90d2f4bf0052ae7
IceSentry <c.giguere42@gmail.com>
2017-02-04 16:11:41 -0500

    camera activity

---
+++ Project/app/src/main/java/com/salty/potholefinder/CameraActivity.java
@@ -0,0 +1,34 @@
+package com.salty.potholefinder;
+
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class CameraActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        Button takePictureButton = (Button)findViewById(R.id.takePictureButton);
+
+        takePictureButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+}
