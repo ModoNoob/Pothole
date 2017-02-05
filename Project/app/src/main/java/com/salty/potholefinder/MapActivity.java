b574a76a4cc4916520ff2a3d9e6d118b00f2b8dc
BobWithHair <matt20four@gmail.com>
2017-02-04 21:10:19 -0500

    fab with toast

--- Project/app/src/main/java/com/salty/potholefinder/MapActivity.java
+++ Project/app/src/main/java/com/salty/potholefinder/MapActivity.java
@@ -1,7 +1,12 @@
 package com.salty.potholefinder;
 
+import android.graphics.Matrix;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MapActivity extends AppCompatActivity {
 
@@ -9,5 +14,18 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        final FloatingActionButton fabBtn = (FloatingActionButton) this.findViewById(R.id.fab);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fabOnClick(v);
+            }
+        });
+    }
+
+    private void fabOnClick(View v){
+        Toast.makeText(this, "WOW", Toast.LENGTH_LONG).show();
     }
 }
